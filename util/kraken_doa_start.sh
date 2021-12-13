74c4a3ab13a00e4a8c5ef75c2178c49a0beaea6a
krakenrf <78108016+krakenrf@users.noreply.github.com>
2021-12-13 17:55:21 +1300

    Update kraken_doa_start.sh

--- util/kraken_doa_start.sh
+++ util/kraken_doa_start.sh
@@ -1,8 +1,8 @@
 ./kraken_doa_stop.sh
 
 cd heimdall_daq_fw/Firmware
-#sudo ./daq_synthetic_start.sh 
-sudo ./daq_start_sm.sh
+#sudo ./daq_synthetic_start.sh
+sudo env "PATH=$PATH" ./daq_start_sm.sh
 sleep 2
 cd ../../krakensdr_doa
-sudo nice -n -20 ./gui_run.sh
+sudo env "PATH=$PATH" ./gui_run.sh
