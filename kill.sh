f5a15d795b79d82cf5a9723ed473b7c3a2eb1b41
Carl Laufer <Kraken.rf.inc@gmail.com>
2022-03-31 11:33:52 +0100

    kill node on restart

--- kill.sh
+++ kill.sh
@@ -1,5 +1,6 @@
 #/bin/sh!
 sudo kill -64 $(ps ax | grep "[p]ython3 _UI/_web_interface/kraken_web_interface.py" | awk '{print $1}') 2> /dev/null
 sudo kill -64 $(ps ax | grep "[p]hp" | awk '{print $1}') 2> /dev/null
+sudo pkill -f node
 #sudo pkill -f gunicorn
 
