6af88cf77ef45c3812ef349d48ec3cfb3e2112a3
Mykola Dvornik <mykola.dvornik@gmail.com>
2023-06-11 21:34:15 +0200

    Add rudimentary remote control method
    The least intrusive way to control DoA DSP remotely without WEB GUI is
    to allow clients to read and upload `settings.json` file that contains
    all the DSP settings (not the DAQ settings though). So lets serve
    `settings.json` on port 8081, where `DOA_value.html` is present as
    well. Since both files have dynamic content, lets remove them from the
    repo. `settings.json` would be either re-created on start, if not
    present, or re-used if it is already there. Consequently, `_android_web`
    folder is also unchecked from the repo. It is re-created on start with
    a new name, `_share`, to better reflect its purpose.
    
    Technically, the functionality is enabled by switching from PHP to
    miniserve (https://github.com/svenstaro/miniserve). It not only allows
    for file uploads, but reduces RAM consumption by roughly 20MB.
    
    The remote control functionallity is hidden behind `en_remote_control`
    (defaults to 'false') and is subject to `miniserve` avaliability.
    Disabling remote control also implies server is falling back to PHP for
    compatibility reasons.
    
    Finally, for the sake of responsiveness the polling rate of the
    settings file watcher is reduced to 100 ms.

--- gui_run.sh
+++ gui_run.sh
@@ -3,17 +3,34 @@
 IPADDR="0.0.0.0"
 IPPORT="8081"
 
+SHARED_FOLDER="_share"
+SETTINGS_PATH="${SHARED_FOLDER}/settings.json"
+
+declare -A STATE_TO_MESSAGE=(["true"]="ENABLED" ["false"]="DISABLED")
+REMOTE_CONTROL="false"
+SERVER_BIN="sudo php -S ${IPADDR}:${IPPORT} -t"
+if [ -f "${SETTINGS_PATH}" ] && [ -x "$(command -v miniserve)" ]; then
+    REMOTE_CONTROL=$(jq .en_remote_control ${SETTINGS_PATH})
+    if [ "$REMOTE_CONTROL" = "true" ]; then
+        SERVER_BIN="miniserve -i ${IPADDR} -p ${IPPORT} -P -u -o"
+    fi
+fi
+echo "Remote Control is ${STATE_TO_MESSAGE[$REMOTE_CONTROL]}"
+
 echo "Starting KrakenSDR Direction Finder"
 
 # Use only for debugging
 #sudo python3 _UI/_web_interface/kraken_web_interface.py 2> ui.log &
 
+echo "Web Interface Running at $IPADDR:8080"
 python3 _UI/_web_interface/kraken_web_interface.py 2> ui.log &
 
-# Start PHP webserver to interface with Android devices
-echo "Web Interface Running at $IPADDR:8080"
-echo "PHP Data Out Server Running at $IPADDR:$IPPORT"
-sudo php -S $IPADDR:$IPPORT -t _android_web 2> /dev/null &
+# Create folder, if it does not exists, that will contain data shared with clients
+mkdir -p "${SHARED_FOLDER}"
+
+# Start webserver to share output and settings with clients
+echo "Data Out Server Running at $IPADDR:$IPPORT"
+$SERVER_BIN "${SHARED_FOLDER}" 2> server.log &
 
 # Start nodejs server for KrakenSDR Pro App
 node _nodejs/index.js  1> /dev/null 2> /dev/null &
--- util/setup_init.sh
+++ util/setup_init.sh
@@ -1,14 +1,15 @@
-#/bin/bash!
+#!/bin/bash
 
-# PhP for android device support
-sudo apt install php7.4-cli
+# miniserve for sharing data with clients over HTTP
+sudo apt-get -y install rustc cargo
+cargo install miniserve
 
 echo "Set execution rights"
 sudo chmod +x ../gui_run.sh
 sudo chmod +x ../kill.sh
 echo "Install dependencies"
 python3 -m pip install numpy
-python3 -m pip install scipy
+python3 -m pip install scipy==1.9.3
 python3 -m pip install pyargus
 python3 -m pip install matplotlib
 
