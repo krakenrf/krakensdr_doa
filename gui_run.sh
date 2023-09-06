2462e5a25fb1c7d6b2cb8e98eaf8b6e9755a41b7
krakenrf <78108016+krakenrf@users.noreply.github.com>
2023-09-06 21:44:16 +1200

    fix running on virtual box virtual machine
    for some reason the folder needs to be created before the code, and there needs to be a delay. Otherwise Ubuntu in Virtual Box throws Input/Output errors.

--- gui_run.sh
+++ gui_run.sh
@@ -29,10 +29,13 @@ echo "Starting KrakenSDR Direction Finder"
 # Use only for debugging
 #sudo python3 _UI/_web_interface/kraken_web_interface.py 2> ui.log &
 
-echo "Web Interface Running at $IPADDR:8080"
-python3 _UI/_web_interface/app.py 2>ui.log &
 # Create folder, if it does not exists, that will contain data shared with clients
 mkdir -p "${SHARED_FOLDER}"
+# In virtual box there needs to be a delay between folder creation and the code
+sleep 0.1
+
+echo "Web Interface Running at $IPADDR:8080"
+python3 _UI/_web_interface/app.py 2>ui.log &
 
 # Start webserver to share output and settings with clients
 echo "Data Out Server Running at $IPADDR:$IPPORT"
