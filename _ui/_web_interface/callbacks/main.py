4a79c4b4dc8cd87c59e89a714f0aeadcb17975c8
krakenrf <krakenrf@krakensdr.com>
2024-02-25 04:53:03 +0000

    remove unneeded callback

--- _ui/_web_interface/callbacks/main.py
+++ _ui/_web_interface/callbacks/main.py
@@ -824,22 +824,6 @@ def toggle_basic_daq(toggle_value):
         return {"display": "none"}
 
 
-@app.callback(
-    [Output("url", "pathname")],
-    [
-        Input("daq_cfg_files", "value"),
-        Input("placeholder_recofnig_daq", "children"),
-        Input("placeholder_update_rx", "children"),
-    ],
-)
-def reload_cfg_page(config_fname, dummy_0, dummy_1):
-    web_interface.daq_ini_cfg_dict = read_config_file_dict(config_fname)
-    web_interface.tmp_daq_ini_cfg = web_interface.daq_ini_cfg_dict["config_name"]
-    web_interface.needs_refresh = False
-
-    return ["/config"]
-
-
 @app.callback(Output("system_control_container", "style"), [Input("en_system_control", "value")])
 def toggle_system_control(toggle_value):
     web_interface.en_system_control = toggle_value
